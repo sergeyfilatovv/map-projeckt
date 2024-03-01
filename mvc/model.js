f739f921396bece97ce01a94b8d1f2cea28e0f23
sergey_filatov <don.filato2010@yandex.ru>
2024-03-01 12:04:06 +0300

    load change madal.js

--- mvc/model.js
+++ mvc/model.js
@@ -2,7 +2,7 @@ const data = [
     {
         title: 'Церковь царственных страстотерпцев',
         subtitle: 'Православный храм',
-        img: '../img/app/church-img.jpg',
+        img: './img/app/church-img.jpg',
         desc: 'Церковь',
         id:1
     },
@@ -16,28 +16,28 @@ const data = [
     {
         title: 'Детский сад Веснушки Комбинированного Вида п. Дубки Саратовского района Саратовской области',
         subtitle: 'Детский сад, ясли',
-        img: '../img/app/kindergarten-img.jpg',
+        img: './img/app/kindergarten-img.jpg',
         desc: 'Школа',
         id:3
     },
     {
         title: 'Средняя общеобразовательная школа п. Дубки Саратовского района Саратовской области',
         subtitle: 'Общеобразовательная школа',
-        img: '../img/app/school-img.jpg',
+        img: './img/app/school-img.jpg',
         desc: 'Детский сад',
         id:4
     },
     {
         title: 'Памятник ветеранам труда',
         subtitle: 'Памятник',
-        img: '../img/app/monument-img.jpg',
+        img: './img/app/monument-img.jpg',
         desc: 'Памятник',
         id:5
     },
     {
         title: 'Территориальное управление Дубковское департамента Гагаринского административного района муниципального образования город Саратов',
         subtitle: 'Администрация',
-        img: '../img/app/admin-img.jpg',
+        img: './img/app/admin-img.jpg',
         desc: 'Администрация',
         id:6
     }
